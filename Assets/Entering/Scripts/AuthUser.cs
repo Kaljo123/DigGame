833e6091d46a43550112690e76a34f4ea7814a6f
Kaljo <nkaljo@gmail.com>
2015-06-20 01:47:59 +0200

    first push

---
+++ Assets/Entering/Scripts/AuthUser.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class AuthUser : MonoBehaviour {
+
+	//000webhost return after echo
+	//<!-- Hosting24 Analytics Code -->
+
+	public Canvas canvas;
+	
+#if UNITY_EDITOR
+	private new static string name = "nkaljo@gmail.com";
+#elif UNITY_ANDROID
+	private new static string name = null;
+	private AndroidJavaObject email = null;
+	private AndroidJavaObject activityContext = null;
+#endif
+	private string scriptURL = "http://mydb.freeiz.com/scripts/RegLog.php";//"localhost/UnityGrave/RegLog.php";
+
+	// Use this for initialization
+	void Start () {
+#if !UNITY_EDITOR
+		if (email == null) {
+			using (AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer")) {
+				activityContext = activityClass.GetStatic<AndroidJavaObject> ("currentActivity");
+			}
+
+			using(AndroidJavaClass pluginClass = new AndroidJavaClass("com.kaljo.unity.Email")){
+				if (pluginClass !=null){
+					email = pluginClass.CallStatic<AndroidJavaObject>("instance");
+					email.Call ("setContext", activityContext);
+					name = email.Call<string>("getEmail");
+				}
+
+			}
+		}
+#endif
+		StartCoroutine ("regLog");
+	}
+
+	IEnumerator regLog(){
+		WWWForm form = new WWWForm ();
+		form.AddField ("email", name);
+
+		WWW script = new WWW (scriptURL, form);
+		yield return script;
+
+		if (script.error != null) {
+			//please check your internet connection or something like that
+			Debug.Log (script.error);
+		} else {
+			//Debug.Log(script.text);
+			if (script.text.StartsWith("registered")){ //registered for first time
+				canvas.gameObject.SetActive(true);
+			}else if (script.text.StartsWith("logined")){ //logined with active username
+				//Debug.Log("level loaded login");
+				Application.LoadLevel("GameScene");
+			}else{ //registered but didn't set username yet
+				canvas.gameObject.SetActive(true);
+			}
+		}
+
+	}
+
+	public static string getEmail(){
+		return name;
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Game/Scripts/Scroll.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll : MonoBehaviour {
+
+	private Touch t;
+	public float limitLeft = -1000;
+	public float limitRight = 1000;
+	
+	void Update(){
+		
+		if (Input.touchCount > 0) {
+			t = Input.touches[0];
+			
+			if (t.phase == TouchPhase.Moved){
+				
+				gameObject.transform.position -= new Vector3(t.deltaPosition.x,0,0) * Time.deltaTime;
+				if (gameObject.transform.position.x > limitRight){
+					gameObject.transform.position = new Vector3(limitRight, gameObject.transform.position.y, gameObject.transform.position.z);
+				}else if (gameObject.transform.position.x < limitLeft){
+					gameObject.transform.position = new Vector3(limitLeft, gameObject.transform.position.y, gameObject.transform.position.z);
+				}
+			}
+		}
+	}
+
+}
